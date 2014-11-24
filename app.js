7b7f0bed385d44f0d4222294ec428e4d82fb89c1
Noémie Lehuby <noemie.lehuby@gmail.com>
2014-11-24 23:02:08 +0100

    suppression de l'API key navitia du repo

--- app.js
+++
@@ -1,3 +0,0 @@
-var navitia_api_key = "ff9b1dd3-30e7-4000-9476-131a38ab7f88"
-var navitia_coverage = "fr-idf"
-
---
+++ default.params.js
@@ -0,0 +1,3 @@
+var navitia_api_key = "<get your very own token at auth.navitia.io>"
+var navitia_coverage = "fr-idf"
+
