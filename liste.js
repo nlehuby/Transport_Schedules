34027cac80ef7290102a2a4c4a590046e55bb8a3
Noémie Lehuby <noemie.lehuby@gmail.com>
2015-04-18 20:29:58 +0200

    bouton de suppression plus petit

--- liste.js
+++ liste.js
@@ -31,10 +31,10 @@ function List_FH_from_localStorage()
       fiche_horaire = $.parseJSON(localStorage[localStorage.key(i)]);
       var div_list = document.createElement("div");
       div_list.className = "div_list";
-      div_list.innerHTML = '<p><a href="FH.html?route='+ fiche_horaire.code_route +'&arret='+ fiche_horaire.code_arret +'">' + fiche_horaire.arret + '</a></p>'
+      div_list.innerHTML = '<p><a href="FH.html?route='+ fiche_horaire.code_route +'&arret='+ fiche_horaire.code_arret +'">' + fiche_horaire.arret + '</p>'
         + '<p>' + fiche_horaire.ligne + '</p>'
-        + '<p>' + fiche_horaire.direction + '</p>'
-        + '<p>' + '<a href="#" id="'+localStorage.key(i)+'"> Supprimer </a>' + '</p>' ; // TODO : idéalement, utiliser un appui long plutôt qu'un lien
+        + '<p>' + fiche_horaire.direction + '</a></p>'
+        + '<p>' + '<a href="#" id="'+localStorage.key(i)+'"> <font size="2">Supprimer</font></a>' + '</p>' ; // TODO : idéalement, utiliser un appui long plutôt qu'un lien
       document.getElementById("liste").appendChild(div_list);
     }
   }
