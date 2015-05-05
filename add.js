6dc53a16adea3d4707c7d9a4c6eee22c12b73d65
Noémie <noemie.lehuby@centrale-marseille.fr>
2015-05-05 17:40:59 +0200

    simplification de l'IHM
    suppression du bouton servant à afficher les parcours : on peut les afficher dès que l'utilisateur a sélectionné un arrêt

--- add.js
+++ add.js
@@ -34,17 +34,8 @@ $(document).ready(function(){
          minLength: 3,
          select: function(event, ui){
              code_arret = ui.item.id;
-             }
-       
-   });
-});
-
-    /* affichage des routes */
-   $("#addbutton").on('click', function () {      
-           if ($('#stop_area_search').val()!='')
-            {  
              //vider la liste des routes
-                document.getElementById("directions_list").innerHTML = '';
+             document.getElementById("directions_list").innerHTML = '';
                 
              //appeler navitia et afficher les résultats
              console.log('appel navitia : récupération des lignes et directions')
@@ -55,17 +46,18 @@ $(document).ready(function(){
                     success: function( data ) {
                        //console.log(data) // DEBUG
                        for (var i = 0; i < data['routes'].length; i++) {
-                       console.log(data['routes'][i])
                           var elem_dir = document.createElement("div");
                           elem_dir.innerHTML ='<a href="FH.html#fromadd?route='+ data['routes'][i]['id'] +'&arret='+  code_arret +'">Ligne '+ data['routes'][i]['line']['network']['name'] + ' '+data['routes'][i]['line']['code'] +", direction "+ data['routes'][i]['direction']['name'] +'</a>';
                           elem_dir.className = "elem_dir";
                           document.getElementById("directions_list").appendChild(elem_dir);
+                          }
                        }
-                       }
-                    });
+                    });             
+             }
+       
+   });
+});
 
-            }
-    });
 
 var $list = document.getElementById("list");
 $list.onclick = function(){
